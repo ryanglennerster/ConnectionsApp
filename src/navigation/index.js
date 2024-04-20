dff555c18b909da906e057c50371c79378b3967b
Ryan Glennerster <ryanglennerster@Ryans-MacBook-Pro.local>
2024-04-20 08:35:58 +0100

    Set up more navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../screens/HomeScreen';
 import SignInScreen from '../screens/SignInScreen';
@@ -8,10 +9,25 @@ import SignUpScreen from '../screens/SignUpScreen';
 import ConfirmEmailScreen from '../screens/ConfirmEmailScreen';
 import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
 import NewPasswordScreen from '../screens/NewPasswordScreen';
+import ChatDetailsScreen from '../screens/ChatDetailsScreen/Index';
+import ChatScreen from '../screens/ChatScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const Navigation = () => {
+
+    const HomeTabs = () => {
+        return (
+            <Tab.Navigator>
+                <Tab.Screen name='Home' component={HomeScreen} />
+                <Tab.Screen name='Chat' component={ChatScreen} />
+                <Tab.Screen name='Profile' component={ProfileScreen} />
+            </Tab.Navigator>
+        )
+    }
+
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={{headerShown: false}}>
@@ -21,7 +37,9 @@ const Navigation = () => {
                 <Stack.Screen name='ForgotPassword' component={ForgotPasswordScreen} />
                 <Stack.Screen name='NewPassword' component={NewPasswordScreen} />
 
-                <Stack.Screen name='Home' component={HomeScreen} />
+                <Stack.Screen name='Home' component={HomeTabs} />
+
+                <Stack.Screen name='ChatDetails' component={ChatDetailsScreen} options={{presentation: "modal"}} />
             </Stack.Navigator>
         </NavigationContainer>
     )
